a6eb5e5c93332ba49b335c79d28b249fa2934f9a
Titanfall god <faris.srnja2004@gmail.com>
2024-02-21 15:51:41 +0100

    Array: deleteAndRealign

--- Arrays/Main.java
+++ Arrays/Main.java
@@ -1,7 +1,12 @@
 package Arrays;
 
 public class Main {
+    static int[] passArray = {1,2,3,15,5,6,7,8,9,15,11,12,13,14,15,16,17,18,19,20};
     public static void main(String[] args){
-        
+        SingleDimensionArray array = new SingleDimensionArray(passArray);
+        array.deleteAndRealignRecursive(15);
+        array.deleteAndRealignItterative(1);
+        array.traverseArray();
     }
+
 }
--- Arrays/SingleDimensionArray.java
+++ Arrays/SingleDimensionArray.java
@@ -1,5 +1,7 @@
 package Arrays;
 
+import java.lang.reflect.Array;
+
 public class SingleDimensionArray {
     int arr[] = null;
 
@@ -8,6 +10,10 @@ public class SingleDimensionArray {
         for (int i = 0; i < sizeOfArray; i++) {
             arr[i] = Integer.MIN_VALUE;
         }
+    }
+
+    public SingleDimensionArray(int[] array){
+        this.arr = array;
     }
 
     public void insert(int location, int valueToBeInserted) {
@@ -22,20 +28,86 @@ public class SingleDimensionArray {
             System.out.println(e.getMessage());
         }
     }
-    public void traverseArray(){
+
+    public void traverseArray() {
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
     }
-    public void sequentialSearch(int target){
+
+    public void sequentialSearch(int target) {
+        try {
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] == target) {
+                    System.out.println("The target element was found at the index " + i);
+                    return;
+                }
+            }
+            System.out.println("The target element was not found in the array");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        ;
+
+    }
+
+    public int deleteValueFromArray(int target) {
+        try {
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] == target) {
+                    arr[i] = Integer.MIN_VALUE;
+                    System.out.println("The target element was deleted");
+                    return i;
+                }
+            }
+            System.out.println("The target element was not found in the array");
+            return -1;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return -1;
+        }
+    }
+
+    public int deleteAndRealignRecursive(int target) {
+        int val;
+        int totalDeleted = 0;
+        do {
+            val = deleteValueFromArray(target);
+            if (val != -1) {
+                totalDeleted++;
+                realignArray(val);
+            }
+        } while (val != -1);
+        
+        return totalDeleted > 0 ? totalDeleted : -1;
+    }
+    
+    private void realignArray(int index) {
+        if (index < arr.length - 1) {
+            if (arr[index] == Integer.MIN_VALUE) {
+                arr[index] = arr[index + 1];
+                arr[index + 1] = Integer.MIN_VALUE;
+            }
+            realignArray(index + 1);
+        }
+    }
+    public int deleteAndRealignItterative(int target){
+        int writeIndex = 0;
+        int deletedCount = 0; 
+        
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == target) {
-                System.out.println("The target element was found at the index " + i);
-                return;
+            if (arr[i] != target) {
+                arr[writeIndex++] = arr[i];
+            } else {
+                deletedCount++;
             }
         }
-        System.out.println("The target element was not found in the array");
-        return;
-
+    
+        int newSize = arr.length - deletedCount;
+        int[] newArr = new int[newSize];
+        System.arraycopy(arr, 0, newArr, 0, newSize);
+        arr = newArr;
+        
+        return deletedCount > 0 ? deletedCount : -1;
     }
 }
